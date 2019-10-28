ade64c793b72e48fc07abab11e3cc4c69e11646f
dmitry_m <press8328@protonmail.com>
2019-10-28 21:34:51 +0700

    add

--- postgree.sh
+++ postgree.sh
@@ -2,3 +2,8 @@ sudo apt-get update
 sudo apt-get install python3-pip python3-dev libpq-dev postgresql postgresql-contrib
 sudo -i -u postgres
 psql
+
+#     dump
+# pg_dump db_name > db_dump.sql
+#     import
+# psql -h 127.0.0.1 -d db_name -U user -f db_dump.sql
